aa74d3968b977eef69debd8369c4d72caa9b7d10
parryhottermagic <johannes.nemeth.2@gmail.com>
2018-03-10 18:06:35 +0100

    Connecting react to redux

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 import Layout from "./Layout";
+import {connect} from "react-redux";
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div>
       <Layout  />
+      {
+        this.props.name.name
+      }
       </div>
     );
   }
@@ -16,6 +20,18 @@ export default class App extends Component {
 const mapStateToProps = (state) => {
   return {
   math: state.math,
-  user: state.user
+  name: state.name
 };
 };
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  changeName: (name) => {
+dispatch({
+type: "CHANGE_NAME",
+payload: name
+});
+}}
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
